193b2e01161d058782d8a3a7700031a9f6042ad3
Lance French <lfrench@pivotal.io>
2018-06-21 11:47:53 -0700

    Fixes some Intellij newline complaints

--- 01-hello-world/main.py
+++ 01-hello-world/main.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 # https://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#PORT
 port = int(os.getenv("PORT"))
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World! I am running on port ' + str(port)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
-
