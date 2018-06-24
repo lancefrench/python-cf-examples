cf3f1132c4b0dc51b470b5b1fda1a6b0b1c82e93
Lance French <lfrench@pivotal.io>
2018-06-24 10:00:30 -0700

    Adds simple message publishing.

--- 02-services-rabbitmq/main.py
+++ 02-services-rabbitmq/main.py
@@ -13,7 +13,16 @@ port = int(os.getenv("PORT"))
 def hello_rabbit():
     vcap_services = json.loads(os.getenv('VCAP_SERVICES'))
     amqp_url = vcap_services['p-rabbitmq'][0]['credentials']['protocols']['amqp']['uri']
+
+    connection = pika.BlockingConnection(pika.URLParameters(amqp_url))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
+    connection.close()
     return 'AMQP URL: ' + str(amqp_url)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
